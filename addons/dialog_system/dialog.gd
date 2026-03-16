3eccff19523f96b599e4c766ae0ad8fa0e861c5a
Mona Lisa <malhart28@gmail.com>
2026-03-16 16:28:29 -0500

    ready to publish

--- addons/dialog_system/dialog.gd
+++ addons/dialog_system/dialog.gd
@@ -3,18 +3,10 @@ class_name Dialog
 const DIALOG_SYSTEM = preload("uid://cb2qeh0ro8vw2")
 var NPCS:={}
 # Called when the node enters the scene tree for the first time.
-var dio:Dialog_system2
-func start(parent=self) -> Dialog_system2:
+var dio:Dialog_system
+func start(parent=self) -> Dialog_system:
 	dio=DIALOG_SYSTEM.instantiate()
 	parent.add_child(dio)
 	return  dio
 
-func Character(NPC_NAME,color:Color=Color.WHITE,image:String ="res://addons/dialog_system/placeholder.png") :
-	dio.changed_NPC_name(NPC_NAME)
-	dio.change_image(image)
-	NPCS[NPC_NAME][{
-		"image":image,
-		"color":color,
-	}]
-	return dio
 	
--- addons/dialog_system/dialog_system.gd
+++ addons/dialog_system/dialog_system.gd
@@ -6,143 +6,234 @@ class_name  Dialog_system
 @onready var convo: RichTextLabel = %convo
 @onready var photo: TextureRect = %photo
 @onready var next_convo: Button = %next_convo
-@onready var bg: TextureRect = %bg
+@onready var bg_obj: TextureRect = %bg
 @onready var sound_tool: AudioStreamPlayer = %sound
 
 @onready var menu_object=preload("res://addons/dialog_system/menu.tscn")
 @onready var input_object=preload("res://addons/dialog_system/input.tscn")
 signal input_received(value)
-signal await_convo()
-var show_one_char_at_a_time:=true
-var speed:=1.0
-# convo varibles exposed to users
+signal talking(value)
+var paused:=false
+var current_tween: Tween
+var start:=false
+var dialog_output:=[]:
+	set(value):
+		dialog_output=value
+		start_convo()
+var Characters:={
+	"default":{
+		"color":Color.WHITE,
+		"image":"res://addons/dialog_system/placeholder.png"}
+}
+var typewriter_speed:=30
+var typewriter:=true
+#backward compatibility
 var text:="":
-	set (value):
+	set(value):
 		text=value
-		NPC_Dialog(value)
-var background:="":
-	set (value):
-		background=value
-		change_background(value)
-var sound:="":
-	set (value):
-		sound=value
-		change_sound(value)
-var user_input:="ggg"
-
-var image:String:
-	set (value):
-		image=value
-		_image_changed(value)
-var npc_name:="tom":
-	set (value):
+		old_text(value)
+#usable varibles 
+var npc_name:="":
+	set(value):
 		npc_name=value
-		NPC_Name_change(value)
-var dialogue_lines:Array=[]
-var current_line = 0
-var menu_inst:Menu
-var input_inst:Dailog_Input
-var choicess:={}
-var is_active:=false
+		changed_NPC_name(value)
+
+var image:="":
+	set(value):
+		image=value
+		change_image(value)
+		
 
 func _ready() -> void:
+	start=true
 	npc.text=npc_name
-	
 	hide()
-func change_sound(value):
-	await await_convo
-	print(sound_tool.stream)
-	sound_tool.stream=load(value)
-	sound_tool.play()
+
+func start_convo():
+	if not paused:
+		start=false
+		proceed()
+
+func add_dialog(type,line):
+	if start:
+		dialog_output=[{type:line}]
+	else:
+		dialog_output.append({type:line})
+
+func changed_NPC_name(value):
+	if Characters.size()<2:
+		Character(value)
+	add_dialog("npc_name",{"name":value})
 	
-func change_background(value):
-	bg.texture=load(value)
-func _image_changed(value):
+		
+		
 
-	photo.texture=load(value)
+func change_image(value):
+	add_dialog("image",{"image":value})
+#Usable functions
+
+func Character(NPC_NAME:String,color:Color=Color.WHITE,image_avatar:String ="res://addons/dialog_system/placeholder.png")->String:
+	Characters[NPC_NAME]={
+		"color":color,
+		"image":image_avatar
+	}
+	return NPC_NAME
+func bg(url:String):
+	add_dialog("bg",{"bg":url})
+
+func voice(url:String,volume_dB:float=0,pitch_scale:float=1):
+	add_dialog("voice",{
+		"url":url,
+		"volume_dB":volume_dB,
+		"pitch_scale":pitch_scale,
+	})
+#old text style (backward compatiblity):
+func old_text(value):
+	add_dialog("text",{
+	"text": value,
+	"typewriter": typewriter,
+	"speed": typewriter_speed,
+	})
+func say(text:String,NPC_name:String=npc_name,typewriter:bool=typewriter,speed:float=typewriter_speed):
+	var current_npc=""
+	if NPC_name=="":
+		current_npc="default"
+	else :
+		current_npc=NPC_name
+	changed_NPC_name(NPC_name)
+	avatar(Characters[current_npc]["image"])
+	add_dialog("text",{
+	"text": text,
+	"typewriter": typewriter,
+	"speed": speed,
+	})
+
+func avatar(value):
+	if image.length()<1:
+		add_dialog("image",{"image":value})
+		
+var user_input:=""
+#Input 
+func input(question:String,userInput:String=""):
+	add_dialog("input",{
+	"question": question,
+	})
+	await input_received
+	next_convo.disabled=false
 	
-func NPC_Name_change(value):
-	npc.text=value
-# Conov part *****
-func NPC_Dialog(text:String):
-	
-	dialogue_lines.append(text)
-	if dialogue_lines[current_line] is String:
-		convo.text = dialogue_lines[current_line]
-		convo.visible_characters=-1
-		if show_one_char_at_a_time:
-			convo.visible_characters=0
-			var tween=create_tween()
-			tween.tween_property(convo,"visible_characters",convo.get_total_character_count(),speed)
-	is_active=true
-	show()
-	await_convo.emit()
-
-func _on_next_convo_pressed() -> void:
-	Check_for_convo()
-# End of Conov part *****
-
-
+	return user_input
 	
 func menu(question:String, choices:Dictionary):
-	if menu_inst:
-			menu_inst.reset()
-	choicess={"question":question,"choices":choices}
-	menu_inst=menu_object.instantiate()
-	menu_inst.choices=choicess
-	add_child(menu_inst)
-	dialogue_lines.append(choicess)
+	add_dialog("menu",{
+	"question": question,
+	"choices": choices,
+	})	
 
+func action(function_name):
+	add_dialog("action", function_name)
+		
+func process_npc_name(key):
+	npc.text=key["npc_name"]["name"]
+	set_Char(key["npc_name"]["name"])
+	move_on(key)
 	
-func input(question:String):
-	input_inst=input_object.instantiate()
+	
+func process_image(key):
+	photo.texture=load(key["image"]["image"])
+	move_on(key)
+
+func process_bg(key):
+	bg_obj.texture=load(key["bg"]["bg"])
+	move_on(key)
+
+func process_voice(key):
+	sound_tool.stream=load(key["voice"]["url"])
+	sound_tool.volume_db=key["voice"]["volume_dB"]
+	sound_tool.pitch_scale=key["voice"]["pitch_scale"]
+	sound_tool.play()
+	move_on(key)
+#text	
+func process_text(key):
+	convo.text=key["text"]["text"]
+	convo.visible_characters=-1
+	
+	if key["text"]["typewriter"]:
+		var speed=convo.get_total_character_count()/key["text"]["speed"]
+		convo.visible_characters=0
+		current_tween=create_tween()
+		current_tween.tween_property(convo,"visible_characters",convo.get_total_character_count(),speed)
+			
+func process_input(key):
+	var input_inst:Dailog_Input=input_object.instantiate()
 	input_inst.user_input_changed.connect(_on_user_input_change)
 	add_child(input_inst)
-	var inputQ={"question":question}
-	input_inst.input_Q=question
-	dialogue_lines.append(inputQ)
-	await input_received
-	return user_input
+	input_inst.show()
+	input_inst.input_Q=key["input"]["question"]
 
 func _on_user_input_change(value):
 	user_input=value
-	next_convo.disabled=false
 	input_received.emit(value)
-	
-	
-func reset():
-		is_active=false
-		next_convo.disabled=false
-		current_line=0
-		hide()
-		dialogue_lines.clear()
-		if menu_inst:
-			menu_inst.reset()
 
-func Check_for_convo():
+func process_menu(key):
+	var choices={"question":key["menu"]["question"],"choices":key["menu"]["choices"]}
+	var menu_inst=menu_object.instantiate()
+	menu_inst.choices=choices
+	add_child(menu_inst)
+	menu_inst.show()
 	
-	current_line += 1
-	next_convo.disabled=false
+func process_action(key):
+	get_parent().call(key["action"])
+	move_on(key)
 	
-	if current_line < dialogue_lines.size():
-		if dialogue_lines[current_line] is String:
-			convo.text = dialogue_lines[current_line]
-			convo.visible_characters=-1
-			if show_one_char_at_a_time:
-				convo.visible_characters=0
-				var tween=create_tween()
-				tween.tween_property(convo,"visible_characters",convo.get_total_character_count(),speed)
+func proceed():
+	if not paused:
+		show()
+		if dialog_output.size()>0:
+			talking.emit(self)
+			var key = dialog_output[0]
+			var type=key.keys()[0]
+			match type:
+				"text":
+					process_text(key)
+				"input":
+					process_input(key)
+					next_convo.disabled=true
+				"menu":
+					process_menu(key)
+					next_convo.disabled=true
+				"image":
+					process_image(key)
+				"bg":
+					process_bg(key)
+				"voice":
+					process_voice(key)
+				"npc_name":
+					process_npc_name(key)
+				"action":
+					process_action(key)
+			dialog_output.erase(key)
 
+			if dialog_output.size()==1:
+				start=true
 		else:
-			next_convo.disabled=true
-			if menu_inst :
-				menu_inst.show()
-				current_line += 1
-			elif input_inst:
-				input_inst.show()
-				current_line += 1
-				
-					
+			hide()
+			start=true
+		
+func _on_next_convo_pressed() -> void:
+	if convo.visible_characters < convo.get_total_character_count():
+		current_tween.kill()
+		convo.visible_characters=convo.get_total_character_count()
 	else:
-		reset()
-	await_convo.emit()
+		proceed()
+	
+func move_on(key):
+	dialog_output.erase(key)
+	proceed()
+func set_Char(NPC_NAME):
+	var current_npc=""
+	if NPC_NAME=="":
+		current_npc="default"
+	else :
+		current_npc=NPC_NAME	
+	convo.add_theme_color_override("default_color",Characters[current_npc]["color"])
+	npc.add_theme_color_override("font_color",Characters[current_npc]["color"])	
